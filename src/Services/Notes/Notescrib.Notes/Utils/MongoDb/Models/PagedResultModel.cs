12df6cd20b01d91415f01614d9656cd90ea836b4
sbotor <botorszym@gmail.com>
2022-12-24 22:57:33 +0100

    Add endpoints for related note management

--- src/Services/Notes/Notescrib.Notes/Utils/MongoDb/Models/PagedResultModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Notescrib.Notes.Utils.MongoDb.Models;
-
-public class PagedResultModel<T>
-{
-    public IReadOnlyCollection<int> Counts { get; set; } = null!;
-    public IReadOnlyCollection<T> Data { get; set; } = null!;
-}
